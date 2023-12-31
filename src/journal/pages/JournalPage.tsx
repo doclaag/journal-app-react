8d47e704f920f88a3c8f6091e82f8c4f7ef7d09d
Luis Alonzo <doclaag@gmail.com>
2023-12-30 22:05:49 -0600

    modified JournalApp and creation of views

--- src/journal/pages/JournalPage.tsx
+++ src/journal/pages/JournalPage.tsx
@@ -1,12 +1,38 @@
-import { Typography } from '@mui/material';
+import { IconButton } from '@mui/material';
 import { JournalLayout } from '../layout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
     return (
         <JournalLayout>
-            <Typography>
+            {/* <Typography>
                 Adipisicing magna voluptate ullamco voluptate veniam voluptate cupidatat ut id culpa elit. Sit et ut aute labore fugiat velit tempor nulla dolor velit. Culpa duis excepteur nulla exercitation et. Irure duis cupidatat esse enim id ea enim sit magna dolor incididunt ex deserunt.
-            </Typography>
+            </Typography> */}
+
+            <NothingSelectedView />
+            {/* <NoteView /> */}
+
+            <IconButton
+                size='large'
+                sx={{
+                    color: 'white',
+                    backgroundColor: 'error.main',
+                    ':hover': {
+                        backgroundColor: 'error.main',
+                        opacity: 0.9
+                    },
+                    position: 'fixed',
+                    right: 50,
+                    bottom: 50,
+                }}
+            >
+                <AddOutlined
+                    sx={{
+                        fontSize: 30,
+                    }}
+                />
+            </IconButton>
         </JournalLayout>
     );
 };
